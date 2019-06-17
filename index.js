8283359f3e20bb589ad28ebc51711a29a595f77b
Danil Shchegol <danilshchegol@icloud.com>
2019-06-17 22:54:49 +0300

    fadf

--- index.js
+++ index.js
@@ -1,63 +1,105 @@
-const TelegramBot = require('node-telegram-bot-api');
+const http = require('http');
+const firebase = require("firebase-admin");
+const serviceAccount = require("./js2019-b705e-firebase-adminsdk-sh4au-1ba1da47b7.json");
 const request = require('request');
 const jsdom = require('jsdom');
-const admin = require("firebase-admin");i
-
 const { JSDOM } = jsdom;
+const webHookUrl = 'https://dareniarbot.danilshchegol.now.sh/';
+const BOT_TOKEN = '850976535:AAHlda5zro82_Gnqz8KBS70I5yBBc-FGoAQ';
 
-const token = '850976535:AAHlda5zro82_Gnqz8KBS70I5yBBc-FGoAQ';
-
-const bot = new TelegramBot(token, {polling: true});
-
-const serviceAccount = require("./js2019-b705e-firebase-adminsdk-sh4au-1ba1da47b7.json");
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://js2019-b705e.firebaseio.com"
-  });
-
-const db = admin.firestore();
-
-bot.onText(/\/start/, function (msg, match) {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Hello! For informations about usage, creator, etc., please use the command /help!');
+firebase.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: "https://js2019-b705e.firebaseio.com"
 });
 
-bot.onText(/\/help/, function (msg, match) {
-    const chatId = msg.chat.id;
-    bot.sendMessage(chatId, 'Usage:\n /imageof <your_word> sends you an random image.\nExample:\n/imageof dog - You\'ll get a random image of a dog.\n\nAll images are taken from https://unsplash.com.\n\n/history - You\'ll get history of your photos.');
-});
+const db = firebase.firestore();
+const photos = db.collection('photos');
 
-bot.onText(/\/imageof (.+)/, function (msg, match) {
-    const word = match[1];
-    const chatId = msg.chat.id;
-    request(`https://unsplash.com/search/photos/${match[1]}`, function (error, response) { 
-        const html = new JSDOM(response.body);
-        const images = Array.from(html.window.document.getElementsByTagName("img"))
-        let sources = [];
-        images.forEach(img => {
-            if (img.src.includes('photo')) {
-                sources.push(img.src);
-            }
-        });
-        const photoURL = sources[Math.floor(Math.random() * sources.length)];
-        bot.sendPhoto(chatId, photoURL);
-        let addDoc = db.collection('photos').add({
-            url: photoURL
-            }).then(ref => {
-            console.log('Added document with ID: ', ref.id);
-            });
+const sendMessage = (chat_id, text, res) => {
+    const sendMessageUrl = `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`;
+  
+    request.post({
+        url: sendMessageUrl,
+        method: 'post',
+        body: {
+          chat_id: chat_id,
+          text: text
+        },
+        json: true
+      },
+      (error, response, body) => {
+        //console.log(error);
+        console.log(body);
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end()
+      }
+    )
+  };
+  
+  http.createServer(function (req, res) {
+    let data = '';
+  
+    req.on('data', chunk => {
+      data += chunk;
     });
-});
-
-bot.onText(/\/history/, function (msg, match) {
-    const chatId = msg.chat.id;
-    let photosRef = db.collection('photos').get().then(snapshot => {
-        snapshot.forEach(doc => {
-            bot.sendPhoto(chatId, doc.data().url)
-            console.log(doc.id, '=>', doc.data());
-        });
-      })
-});
-
-const sendPhoto = (msg, url) => msg.reply.photo(url);
+  
+    req.on('end', () => {
+      const parsedUpdate = data != "" ? JSON.parse(data) : {};
+      if (typeof parsedUpdate.message !== 'undefined') {
+        const text = parsedUpdate.message.text;
+        const chat_id = parsedUpdate.message.chat.id;
+        console.log(parsedUpdate);
+        if (text === '/help') {
+          const reply = 'Usage:\n /imageof <your_word> sends you an random image.\nExample:\n/imageof dog - You\'ll get a random image of a dog.\n\nAll images are taken from https://unsplash.com.\n\n/history - You\'ll get history of your photos.'
+          sendMessage(chat_id, reply, res)
+        } else if (text === '/history') {
+            const reply = 'Usage:\n /imageof <your_word> sends you an random image.\nExample:\n/imageof dog - You\'ll get a random image of a dog.\n\nAll images are taken from https://unsplash.com.\n\n/history - You\'ll get history of your photos.'
+          sendMessage(chat_id, reply, res)
+        //   let photosRef = photos.limit(5).get().then(snapshot => {
+        //       let urls = ''
+        //       snapshot.forEach(doc => {
+        //           urls += doc.data().url + '\n'
+        //       });
+        //       sendMessage(chat_id, urls, res)
+        //   })
+        } else if (text.match(/\/imageof (.+)/) != null) {
+          const word = text.match(/\/imageof (.+)/)[1]
+          request(`https://unsplash.com/search/photos/${word}`, function (error, response) { 
+              const html = new JSDOM(response.body);
+              const images = Array.from(html.window.document.getElementsByTagName("img"))
+              let sources = [];
+              images.forEach(img => {
+                  if (img.src.includes('photo')) {
+                      sources.push(img.src);
+                  }
+              });
+              const photoURL = sources[Math.floor(Math.random() * sources.length)];
+              console.log(photoURL)
+              sendMessage(chat_id, photoURL, res)
+              let addDoc = db.collection('photos').add({
+                  url: photoURL
+                  }).then(ref => {
+                  console.log('Added document with ID: ', ref.id);
+                  });
+          });
+        }
+      }
+    });
+  }).listen(3000);
+  
+  
+  const setWebHook = () => {
+    const setWebhookUrl = `https://api.telegram.org/bot${BOT_TOKEN}/setWebhook`;
+  
+    request.post({
+      url: setWebhookUrl,
+      method: 'post',
+      body: {
+        url: webHookUrl
+      },
+      json: true
+    },
+    (error, response, body) => {
+      console.log(body);
+    })
+  };
